a8e3184d02b87281cb50623e0619a70554ea7e90
Sujay Menon <menonsujay011@gmail.com>
2024-08-21 15:51:46 +0530

    test1

--- src/data/index.js
+++ src/data/index.js
@@ -28,24 +28,6 @@ export const navLinks = [
 const experiences = [
   {
     title: "Data Analyst",
-<<<<<<< HEAD
-    company_name: "Employed at VNJ Innovations",
-    date: "July 2024 - Present",
-    details: [
-      "Performed Data Analysis on large data with over <span style='color: white;'>100,000 entries</span> as well as cleaned the data, and used visualization tools like Looker Studio to present data to stakeholders.",
-      "Crafted visually appealling website with over <span style='color: white;'>20 pages</span>.",
-      "Proven ability to <span style='color: white;'>translate complex analytical findings</span> into clear and actionable recommendations.",
-      "Produced reports based on Data collected by a streamline methods created by myself to better the existing performance for <span style='color: white;'>Sales and Medicine</span>, which ultimately helped progress.",
-    ],
-  },
-  {
-    title: "Natural Language Processing",
-    company_name: "Cymbeline Innovatiions",
-    date: "Jan 2024 - Mar 2024",
-    details: [
-      "Developed and delivered <span style='color: white;'> custom Machine Learning models</span> as per requested by the project leader.",
-      "<span style='color: white;'>Designed and developed innovative</span> AI applications.",
-=======
     company_name: "VNJ Innovations",
     date: "Jul 2024 - Present",
     details: [
@@ -61,7 +43,6 @@ const experiences = [
     details: [
       "Created and trained <span style='color: white;'>multiple machine learning models</span> that convert natural language to SQL, and subsequently leveraged all NLP techniques to better the models that were created, and were tested on a custom dataset. ",
       "Helped in <span style='color: white;'>Designing and developing innovative</span> AI applications.",
->>>>>>> 7f93c9c (Done)
       "<span style='color: white;'>Managed full project lifecycle</span> from concept to deployment in successful and timely project completions.",
     ],
   },
@@ -70,30 +51,17 @@ const experiences = [
     company_name: "Repute",
     date: "Jan 2022 - Feb 2023",
     details: [
-<<<<<<< HEAD
-      "Led a team of 20 and successfully coordinated high-profile events such as music, dancing and other types of recreational events while contributing to the creation of revenue-generating marketing visual content and <span style='color: white;'>showcasing exceptional leadership skills</span>.",
-      "Utilised Raw Data of past events to better the quality of future events and also <span style='color: white;'>predict the qualitative and quantitative parameters</span> of the audience.",
-      "<span style='color: white;'>Gathered and managed data</span> both primary and secondary , as well as created visual representations using python to convey thee findings to non-technical personnel",
-=======
       "<span style='color: white;'>Led a team of 20</span> and successfully coordinated high-profile events such as music, dancing and other types of recreational events while contributing to the creation of revenue-generating marketing visual content and showcasing exceptional leadership skills. ",
       " worked as data analyst and <span style='color: white;'>analysed raw data of past events</span> to better the quality of future events and also predict the qualitative and quantitative parameters of the audience.",
       "Visualized data for all the ongoing events <span style='color: white;'>using visualizing tools such as Looker Studio</span> and conducted a benchmark analysis",
->>>>>>> 7f93c9c (Done)
     ],
   },
   {
     title: "Data Analyst Intern",
-<<<<<<< HEAD
-    company_name: "Campalin Innovations",
-    date: "2015 - 2018",
-    details: [
-      "Developed and <span style='color: white;'>executed an efficient Intelligent System</span> for various purposes which include but are not limited to Loan Prediction using Machine Learning, Housing Prices Prediction Project, Stock Price Prediction using Machine Learning, Titanic Survival Project",
-=======
     company_name: "Campallin Innovations",
     date: "Aug 2022 - Oct 2022",
     details: [
       "<span style='color: white;'>Developed and executed efficient Intelligent Systems</span> for various purposes which include but are not limited to Loan Prediction using Machine Learning, Housing Prices Prediction Project, Stock Price Prediction using Machine Learning, Titanic Survival Project. ",
->>>>>>> 7f93c9c (Done)
     ],
   },
 ];
@@ -102,22 +70,6 @@ const portfolio = [
   {
     name: "Sign Language to Audio Project",
     description:
-<<<<<<< HEAD
-      "A Project in which I created a website which converts Sign Language, from a live feed camera to convert it into text and audio both in realtime.",
-    image: oscs,
-  },
-  {
-    name: "Website",
-    description:
-      "Created a website for a Data Analyst company which was visually appealing and informative.",
-    image: devnotes,
-  },
-  {
-    name: "Courses Completed",
-    description:
-      "Completed various courses on coursera which include but are not limited to Artificial Intelligence, Machine Learning, 90 days of Python Bootcamp, etc.",
-    image: algorithms,
-=======
       "Created a website which converts sign language to text and audio in realtime, using the feed from the camera efficiently.",
     image: sc,
   },
@@ -132,7 +84,6 @@ const portfolio = [
     description:
       "Completed various courses on coursera which include but are not limited to Artificial Intelligence, Machine Learning, 90 days of Python Bootcamp, etc.",
     image: pythonbootcamp,
->>>>>>> 7f93c9c (Done)
   },
 ];
 
